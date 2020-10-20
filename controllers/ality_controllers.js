4cb85172aa34f63121b356624b8bfe52efa15a32
Quint Turner <henryquintturner@gmail.com>
2020-10-20 10:19:15 -0700

    Made basic MVC file structure

---
+++ controllers/ality_controllers.js
@@ -0,0 +1,10 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var db = require("../models/");
+
+//Build routes here!!!
+
+module.exports = router;
