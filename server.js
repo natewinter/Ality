4f3fe5d4967eec69baee17ff9743a1d5e3977648
caitlinbou <caitlin.bouroncle@gmail.com>
2020-10-20 13:54:47 -0700

    update schema, build user.js

--- server.js
+++ server.js
@@ -18,13 +18,13 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/ality_controllers.js");
+const routes = require("./controllers/ality_controllers.js");
 app.use(routes);
 
 // Start server with sequelize so that it can begin listening to client requests.
-db.sequelize.sync({ force: true}).then=()=>{
+// db.sequelize.sync({ force: true}).then=()=>{
 app.listen(PORT, function() {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
-};
+// };
