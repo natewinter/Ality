a076d7c8a4daa9683ef9b8eefd704bca4c1120a3
caitlinbou <caitlin.bouroncle@gmail.com>
2020-10-20 11:27:37 -0700

    server with sequelize

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
-var express = require("express");
-
-var PORT = process.env.PORT || 8080;
-
-var app = express();
-
+// Dependencies
+const express = require("express");
+// Sets port for deploymet and localhost
+const PORT = process.env.PORT || 8080;
+// Sets up app express
+const app = express();
+// establishes db for sequelize
+const db = require("./models");
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
-
-// Parse application body as JSON
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+// Set Handlebars
+const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
-
+var routes = require("./controllers/ality_controllers.js");
 app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
+// Start server with sequelize so that it can begin listening to client requests.
+db.sequelize.sync({ force: true}).then=()=>{
 app.listen(PORT, function() {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
+};
