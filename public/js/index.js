574285bc2c632c04f8fbee9badfadcfb56dc4178
Quint Turner <henryquintturner@gmail.com>
2020-10-22 14:36:53 -0700

    Added users modal

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,5 @@
 $(function() {
+  $(document).foundation();
     $(".login-form").on("submit", function(event) {
         // Make sure to preventDefault on a submit event.
         event.preventDefault();
