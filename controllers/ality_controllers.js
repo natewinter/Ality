e4c74aafc4b6d2d0fc3ff1f0cf2ef91f9ed41904
Quint Turner <henryquintturner@gmail.com>
2020-10-22 11:23:19 -0700

    FINALLY connected users page to backend

--- controllers/ality_controllers.js
+++ controllers/ality_controllers.js
@@ -18,14 +18,34 @@ router.get("/", (req, res) => {
     res.render("index");
 });
 
-router.get("/users/:id", (req, res) => {
+router.get("/users/:name", (req, res) => {
     db.User.findOne({
         where: {
-            id: req.params.id,
+            username: req.params.name,
         }
     }).then(function (dbUser) {
-        console.log(dbUser);
-        return res.render("user", dbUser);
+        if (dbUser) {
+            db.Stat_List.findAll({
+                include: [
+                    {
+                        model: db.User
+                    }
+                ]
+            }).then(dbStat_Lists => {
+                const statListArray = [];
+                for (let index = 0; index < dbStat_Lists.length; index++) {
+                    const element = dbStat_Lists[index].toJSON();
+                    statListArray.push(element);
+                }
+                const nameAndLists = {
+                    user: dbUser.toJSON(),
+                    stat_lists: statListArray
+                }
+                return res.render("user", nameAndLists);
+            });
+        } else {
+            return res.status(404).render("404");
+        }
     });
 });
 
@@ -35,7 +55,7 @@ router.get("/stat-list/:id", (req, res) => {
             id: req.params.id,
         }
     }).then(function (dbStat_List) {
-        if(dbStat_List){
+        if (dbStat_List) {
             db.Data_Value.findAll({
                 include: [
                     {
@@ -45,14 +65,12 @@ router.get("/stat-list/:id", (req, res) => {
                         model: db.Stat_Def
                     }
                 ]
-                
-            }).then(dbData_Values=>{
+
+            }).then(dbData_Values => {
                 console.log(dbStat_List.name);
-                
-                // console.log(dbData_Values);
-                
+
                 let stat_list = AlityHelper.buildStatList(dbStat_List.name, dbData_Values);
-                
+
                 return res.render("stat_list", stat_list);
             });
         } else {
@@ -64,31 +82,35 @@ router.get("/stat-list/:id", (req, res) => {
 //  APIS =====================================================================
 
 router.post("/api/users", function (req, res) {
-  db.User.create({
-      username: req.body.username,
-      email: req.body.email
-  }).then(function (dbUser) {
-      console.log(dbUser);
-      res.redirect("/");
-  });
+    db.User.create({
+        username: req.body.username,
+        email: req.body.email
+    }).then(function (dbUser) {
+        console.log(dbUser);
+        res.redirect("/");
+    });
 });
 router.get("/api/users", function (req, res) {
-  db.User.findAll().then(function (dbUser) {
-    if(!dbUser){
-      return res.status(404).end()
-    };
-      console.log(dbUser);
-      return res.json(dbUser);
-  });
+    db.User.findAll().then(function (dbUser) {
+        if (!dbUser) {
+            return res.status(404).end()
+        };
+        console.log(dbUser);
+        return res.json(dbUser);
+    });
 });
 
 router.post("/api/stat-lists", function (req, res) {
+    console.log("req.body:", req.body);
     db.Stat_List.create({
-        stat_list_name: req.body.stat_list,
+        name: req.body.name,
+        UserId: req.body.UserId
     }).then(function (dbStatlist) {
         console.log(dbStatlist);
         // res.reload();
-        res.redirect("/user")
+        res.json(dbStatlist)
+    }).catch(function (err) {
+        console.log(err);
     });
 });
 
@@ -96,8 +118,8 @@ router.get("/api/stat-lists", function (req, res) {
     db.Stat_List.findAll().then(function (dbStatlist) {
         console.log(dbStatlist);
         // res.reload();
-        if(!dbStatlist){
-          return res.status(404).end()
+        if (!dbStatlist) {
+            return res.status(404).end()
         };
         return res.json(dbStatlist);
     });
@@ -117,9 +139,9 @@ router.post("/api/ality", function (req, res) {
 
 router.get("/api/ality", function (req, res) {
     db.Ality.findAll().then(function (dbAlity) {
-      if(!dbAlity){
-        return res.status(404).end()
-      };
+        if (!dbAlity) {
+            return res.status(404).end()
+        };
         console.log(dbAlity);
         // res.reload();
         return res.json(dbAlity)
@@ -127,50 +149,50 @@ router.get("/api/ality", function (req, res) {
 });
 
 router.post("/api/stat-defs", function (req, res) {
-  db.Stat_Def.create({
-      name: req.body.name,
-      stat_type: req.body.stat_type
-  }).then(function (dbStatDef) {
-      console.log(dbStatDef);
-      // res.reload();
-      res.redirect("/user")
-  });
+    db.Stat_Def.create({
+        name: req.body.name,
+        stat_type: req.body.stat_type
+    }).then(function (dbStatDef) {
+        console.log(dbStatDef);
+        // res.reload();
+        res.redirect("/user")
+    });
 });
 
 router.get("/api/stat-defs", function (req, res) {
-  db.Stat_Def.findAll().then(function (dbStatDef) {
-      console.log(dbStatDef);
-      // res.reload();
-      if(!dbStatDef){
-        return res.status(404).end()
-      };
+    db.Stat_Def.findAll().then(function (dbStatDef) {
+        console.log(dbStatDef);
+        // res.reload();
+        if (!dbStatDef) {
+            return res.status(404).end()
+        };
         console.log(dbStatDef);
         // res.reload();
         return res.json(dbStatDef)
-  });
+    });
 });
 
 router.post("/api/data-values", function (req, res) {
-  db.Data_Value.create({
-      val_A: req.body.val_A,
-      val_B: req.body.val_B
-  }).then(function (dbDataValue) {
-      console.log(dbDataValue);
-      // res.reload();
-      res.redirect("/user")
-  });
+    db.Data_Value.create({
+        val_A: req.body.val_A,
+        val_B: req.body.val_B
+    }).then(function (dbDataValue) {
+        console.log(dbDataValue);
+        // res.reload();
+        res.redirect("/user")
+    });
 });
 
 router.get("/api/data-values", function (req, res) {
-  db.Data_Value.findAll().then(function (dbDataValue) {
-      console.log(dbDataValue);
-      if(!dbDataValue){
-        return res.status(404).end()
-      };
+    db.Data_Value.findAll().then(function (dbDataValue) {
+        console.log(dbDataValue);
+        if (!dbDataValue) {
+            return res.status(404).end()
+        };
         console.log(dbDataValue);
         // res.reload();
         return res.json(dbDataValue)
-  });
+    });
 });
 
 router.get("/api/users/:id", (req, res) => {
